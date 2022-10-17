1aec0d306f3d7072f32fc482d54082caabef8857
Sam <ss7i@outlook.com>
2022-10-17 17:33:26 +0100

    release build

---
+++ release/index.d.ts
@@ -0,0 +1,11 @@
+import { Plugin } from 'vite';
+import browserSync from 'browser-sync';
+
+declare type OptionsType = 'snippet' | 'proxy';
+interface Options {
+    mode?: OptionsType;
+    bs?: browserSync.Options;
+}
+declare function VitePluginBrowserSync(options?: Options): Plugin;
+
+export { Options, VitePluginBrowserSync as default };
